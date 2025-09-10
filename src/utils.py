a94cb5d0013dfa3184dae946196ced6b79ef1877
Prince Kay <princekay145@gmail.com>
2025-09-10 12:17:18 +0300

    hyperparameter training

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import dill
 import numpy as np
 import pandas as pd
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
 from src.logger import logging
 
@@ -16,17 +17,24 @@ def save_object(file_path, obj):
             dill.dump(obj, file_obj)
     except Exception as e:
         raise CustomException(e, sys)
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
-        for i in range(len(list(models))):
-            model=list(models.values())[i]
+        # Iterate by model name to keep keys aligned and robust
+        for model_name, model in models.items():
+            param_grid = params.get(model_name, {})
+
+            gs = GridSearchCV(model, param_grid, cv=3)
+            gs.fit(X_train, y_train)
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
-            y_train_pred = model.predict(X_train)
+
+            # Predictions for evaluation
             y_test_pred = model.predict(X_test)
-            train_model_score = r2_score(y_train, y_train_pred)
+            # Train score computed but not used for selection; omit to avoid linter warning
             test_model_score = r2_score(y_test, y_test_pred)
-            report[list(models.keys())[i]] = test_model_score
+
+            report[model_name] = test_model_score
         return report
     except Exception as e:
         raise CustomException(e, sys)
